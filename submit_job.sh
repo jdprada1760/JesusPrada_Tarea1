b741ae2f27633a9ec96df61f4eb27ac30c88aa10
jdprada1760 <jd.prada1760@uniandes.edu.co>
2017-03-10 15:15:41 -0500

    Nprocs

--- submit_job.sh
+++ submit_job.sh
@@ -1,10 +1,11 @@
 #PBS -N test_scheduler
-#PBS -l nodes=1:ppn=4
+#PBS -l nodes=1:ppn=8
 #PBS -M jd.prada1760@uniandes.edu.co
 #PBS -m abe
 
 module load rocks-openmpi_ib
 cd $PBS_O_WORKDIR
+NPROCS=`wc -l < $PBS_NODEFILE`
 ./a.out Time2.data Ek2.data Chain.data 2
 ./a.out Time4.data Ek4.data Chain.data 4
 ./a.out Time8.data Ek8.data Chain.data 8
